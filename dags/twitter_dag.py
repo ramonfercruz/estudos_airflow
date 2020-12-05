3e873313ba6c2e6ac65e2159cc1ff428a767c76b
ramonfercruz <ramonfer.cruz@gmail.com>
2020-12-05 16:11:44 +0000

    altualização de load_tweets

--- dags/twitter_dag.py
+++ dags/twitter_dag.py
@@ -3,7 +3,7 @@ from airflow.contrib.sensors.file_sensor import FileSensor
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-
+from airflow.operators.hive_operator import HiveOperator
 
 import fetching_tweet, cleaning_tweet
 
@@ -18,3 +18,4 @@ with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_
 	fetching_tweets = PythonOperator(task_id="fetching_tweets", python_callable=fetching_tweet.main)
 	cleaning_tweets = PythonOperator(task_id="cleaning_tweets", python_callable=cleaning_tweet.main)
 	storing_tweets = BashOperator(task_id="storing_tweets", bash_command="hadoop fs -put -f /tmp/data_cleaned.csv /tmp/")
+	loading_tweets = HiveOperator(task_id="loading_tweets", hql="LOAD DATA INPATH '/tmp/data_cleaned.csv' INTO TABLE tweets")
